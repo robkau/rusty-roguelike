82a25c38132c8e45bc64ff455ed10824f469b69c
robkau <robkau@users.noreply.github.com>
2023-02-23 21:57:05 -0500

    page 84

---
+++ src/main.rs
@@ -0,0 +1,48 @@
+#![deny(clippy::all, clippy::pedantic, clippy::restriction, clippy::nursery, clippy::cargo)]
+#![deny(future_incompatible, let_underscore, nonstandard_style, rust_2018_compatibility, rust_2018_idioms, rust_2021_compatibility)]
+#![warn(warnings,unused)]
+
+use bracket_lib::prelude::*;
+
+mod map;
+
+mod prelude {
+    pub use bracket_lib::prelude::*;
+    pub const SCREEN_WIDTH: i32 = 80;
+    pub const SCREEN_HEIGHT: i32 = 50;
+    pub use crate::map::*;
+}
+
+use prelude::*;
+
+
+
+fn main() -> BError {
+    let ctx = BTermBuilder::simple80x50()
+        .with_title("Rusty Roguelike")
+        .with_fps_cap(30.)
+        .build()?;
+
+    main_loop(ctx, State::new())
+}
+
+
+struct State {
+    map: Map
+}
+
+impl GameState for State {
+    fn tick(&mut self, ctx: &mut BTerm) {
+        ctx.cls();
+        self.map.render(ctx);
+    }
+}
+
+
+impl State {
+    fn new() -> Self {
+        State {
+            map: Map::new(),
+        }
+    }
+}
