b3f993420ec0468691baeba1b1a4207c44a95e1a
robkau <robkau@users.noreply.github.com>
2023-02-25 17:52:37 -0500

    .. -> ..= to fix impassible corners.

--- src/map_builder.rs
+++ src/map_builder.rs
@@ -56,7 +56,6 @@ impl MapBuilder {
         }
     }
 
-    // todo sometimes impassible corners without diagonal movement.
     fn build_corridors(&mut self, rng: &mut RandomNumberGenerator) {
         let mut rooms = self.rooms.clone();
         rooms.sort_by(|a, b| a.center().x.cmp(&b.center().x));
@@ -77,7 +76,7 @@ impl MapBuilder {
 
     fn apply_vertical_tunnel(&mut self, y1: i32, y2: i32, x: i32) {
         use std::cmp::{min,max};
-        for y in min(y1,y2) .. max(y1,y2) {
+        for y in min(y1,y2) ..= max(y1,y2) {
             if let Some(idx) = self.map.try_idx(Point::new(x,y)) {
                 self.map.tiles[idx as usize] = TileType::Floor;
             }
@@ -86,7 +85,7 @@ impl MapBuilder {
 
     fn apply_horizontal_tunnel(&mut self, x1: i32, x2: i32, y: i32) {
         use std::cmp::{min,max};
-        for x in min(x1,x2) .. max(x1,x2) {
+        for x in min(x1,x2) ..= max(x1,x2) {
             if let Some(idx) = self.map.try_idx(Point::new(x,y)) {
                 self.map.tiles[idx as usize] = TileType::Floor;
             }
