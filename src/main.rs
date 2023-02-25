53ff65346c19a73e44567820c757a4f5459ad940
robkau <robkau@users.noreply.github.com>
2023-02-25 17:50:51 -0500

    page 94

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,16 @@
 use bracket_lib::prelude::*;
 
 mod map;
+mod map_builder;
+mod player;
 
 mod prelude {
     pub use bracket_lib::prelude::*;
     pub const SCREEN_WIDTH: i32 = 80;
     pub const SCREEN_HEIGHT: i32 = 50;
     pub use crate::map::*;
+    pub use crate::map_builder::*;
+    pub use crate::player::*;
 }
 
 use prelude::*;
@@ -28,21 +32,28 @@ fn main() -> BError {
 
 
 struct State {
-    map: Map
+    map: Map,
+    player: Player,
 }
 
 impl GameState for State {
     fn tick(&mut self, ctx: &mut BTerm) {
         ctx.cls();
+        self.player.update(ctx, &self.map);
         self.map.render(ctx);
+        self.player.render(ctx);
     }
 }
 
 
 impl State {
     fn new() -> Self {
-        State {
-            map: Map::new(),
+        let mut rng = RandomNumberGenerator::new();
+        let map_builder = MapBuilder::new(&mut rng);
+
+        Self {
+            map: map_builder.map,
+            player: Player::new(map_builder.player_start),
         }
     }
 }
