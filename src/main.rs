2fbf266a4dedfd733e7b2ce25238baed2d1c6073
robkau <robkau@users.noreply.github.com>
2023-02-26 11:28:09 -0500

    p. 101 camera and graphic tiles

--- src/main.rs
+++ src/main.rs
@@ -17,6 +17,7 @@
 
 use bracket_lib::prelude::*;
 
+mod camera;
 mod map;
 mod map_builder;
 mod player;
@@ -25,6 +26,9 @@ mod prelude {
     pub use bracket_lib::prelude::*;
     pub const SCREEN_WIDTH: i32 = 80;
     pub const SCREEN_HEIGHT: i32 = 50;
+    pub const DISPLAY_WIDTH: i32 = SCREEN_WIDTH / 2;
+    pub const DISPLAY_HEIGHT: i32 = SCREEN_HEIGHT / 2;
+    pub use crate::camera::*;
     pub use crate::map::*;
     pub use crate::map_builder::*;
     pub use crate::player::*;
@@ -33,9 +37,15 @@ mod prelude {
 use prelude::*;
 
 fn main() -> BError {
-    let ctx = BTermBuilder::simple80x50()
+    let ctx = BTermBuilder::new()
         .with_title("Rusty Roguelike")
         .with_fps_cap(30.)
+        .with_dimensions(DISPLAY_WIDTH, DISPLAY_HEIGHT)
+        .with_tile_dimensions(32, 32)
+        .with_resource_path("resources/")
+        .with_font("dungeonfont.png", 32, 32)
+        .with_simple_console(DISPLAY_WIDTH, DISPLAY_HEIGHT, "dungeonfont.png")
+        .with_simple_console_no_bg(DISPLAY_WIDTH, DISPLAY_HEIGHT, "dungeonfont.png")
         .build()?;
 
     main_loop(ctx, State::new())
@@ -44,14 +54,18 @@ fn main() -> BError {
 struct State {
     map: Map,
     player: Player,
+    camera: Camera,
 }
 
 impl GameState for State {
     fn tick(&mut self, ctx: &mut BTerm) {
+        ctx.set_active_console(0);
         ctx.cls();
-        self.player.update(ctx, &self.map);
-        self.map.render(ctx);
-        self.player.render(ctx);
+        ctx.set_active_console(1);
+        ctx.cls();
+        self.player.update(ctx, &self.map, &mut self.camera);
+        self.map.render(ctx, &self.camera);
+        self.player.render(ctx, &self.camera);
     }
 }
 
@@ -63,6 +77,7 @@ impl State {
         Self {
             map: map_builder.map,
             player: Player::new(map_builder.player_start),
+            camera: Camera::new(map_builder.player_start),
         }
     }
 }
